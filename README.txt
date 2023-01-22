b469a68b5ab0fdd120b9c1b081caec9430481bb3
Kian Ryan <kian@orangetentacle.co.uk>
2023-01-22 22:37:18 +0000

    BCD Packed encoding

---
+++ README.txt
@@ -0,0 +1,9 @@
+
+; https://www.chibiakumas.com/z80/advanced.php
+; https://smallcomputercentral.files.wordpress.com/2018/05/scmon-v1-0-reference-e1-0-0.pdf
+; https://smallcomputercentral.files.wordpress.com/2018/05/scmon-v1-0-userguide-e1-0-0.pdf
+
+;https://archive.org/details/Programming_the_Z-80_2nd_Edition_1980_Rodnay_Zaks/page/523/mode/2up
+; Contains ASCII -> BCD Routine
+; First pass
+
--- calculator.asm
+++ calculator.asm
@@ -6,15 +6,21 @@
 
 org $8000
 
+read_buf: equ $9000
+
 main:
-	call read_encode_number	; Read number
+	call read_encode_number		
+	; call read_char_display_char	; Read number
+ret
 
 
 read_encode_number:
 	call read_line		; Read line in buffer
-	call encode_number	; Encode ASCII number number to BCD
-	call bcd_display 	; Display BCD as ASCII
-	ret
+	call disp_line		; Display line in memory
+	call encode_bcd		; Encode ASCII number number to BCD
+	call pack_bcd		; Pack BCD
+	; call bcd_display 	; Display BCD as ASCII
+ret
 
 ; SCM Specific Imp read_line
 ; OUT
@@ -22,29 +28,41 @@ read_encode_number:
 ; A = chars in line
 read_line:
 	ld de, $9000		; Start of line, $9000
-	ld a, $51		; Start of line = $50 + 1 for terminator
+	ld a, $52		; Start of line = $50 + 1 for terminator
 	ld c, $04		; Function 4 = Input line
-	rst 30			; Call API
+	rst $30			; Call API
 ret
 
-; https://www.chibiakumas.com/z80/advanced.php
-; https://smallcomputercentral.files.wordpress.com/2018/05/scmon-v1-0-reference-e1-0-0.pdf
-; https://smallcomputercentral.files.wordpress.com/2018/05/scmon-v1-0-userguide-e1-0-0.pdf
+disp_line:
+	push de
+	push af
+		; ld c, $07		; Function 7 = New Line
+		; rst $30			; Call API
+		; ld de, read_buf		; Start of line $9000
+		ld c, $06		; Function 6 = Display line
+		rst $30			; Call API
+	pop af
+	pop de
 
-;https://archive.org/details/Programming_the_Z-80_2nd_Edition_1980_Rodnay_Zaks/page/523/mode/2up 
-; Contains ASCII -> BCD Routine
-; First pass
+; Print individual char to display
+; Input a -> ascii char
+; Output a -> ascii char
+print_char:
+	ld c, $02		; Print individual char
+	rst $30			; Call API
+ret
 
+; moves de by b bytes
 bcd_get_end:
 	push bc
 		ld c,b
 		dec c
 		ld b,0
 		add hl,bc
-		ex hl,de
+		ex de,hl
 		add hl,de
 		add hl,bc
-		ex hl,de
+		ex de,hl
 	pop bc
 ret
 
@@ -53,10 +71,10 @@ bcd_display:
 bcd_show_direct:
 	ld a,(de)
 	and %11110000
-	rrcca
-	rrcca
-	rrcca
-	rrcca
+	rrca
+	rrca
+	rrca
+	rrca
 	add '0'
 	call print_char
 	ld a,(de)
@@ -64,7 +82,7 @@ bcd_show_direct:
 	and %00001111
 	add '0'
 	call print_char
-	djnz BCD_show_direct
+	djnz bcd_show_direct
 ret
 
 ; Encode number held at DE to BCD
@@ -73,14 +91,82 @@ ret
 ; A = chars in line
 
 encode_bcd:
-	ld c, de	; Load address to c
-	add a		; Get end address
+	push hl
+	push de
+	push bc
+	push af
+
+	ex de, hl		; Switch so we can operate on hl
+	ld b, 0
+	ld c, a
+	add hl, bc		; Add a to hl to find end
+
+	ld b, a			; Load b to a for loop
+	inc b			; 
+
+	encode_bcd_loop:
+		call asc_bcd		; Encode char at HL to BCD
+		dec hl			; Dec hl register
+		djnz encode_bcd_loop	; Dec b, jump if non zero
+	pop af
+	pop bc
+	pop de
+	pop hl
+ret
+
+; Pack single byte BCD to double byte
+; BCD
+; input DE = start of memory
+; input A = number of bytes
+pack_bcd:
+	ex de, hl	; Switch to operate on hl
+	ld b, 0
+	ld c, a
+	add hl, bc	; Move to end
+	dec hl
+
+	ld b, a		; Count down for bytes
+	; inc b
+
+	ld de, $9010    ; Set location for write
+
+	pack_bcd_loop:
 	
+		; Read first byte
+		ld c, (hl)
+		dec hl
 
+		; Read second byte, add
+		ld a, (hl)
+		rrca
+		rrca
+		rrca
+		rrca
+		add a, c
+		dec hl
 
+		ld (de), a
+		inc de
+
+		dec b
+		jr z, exit_loop
+
+		dec b
+		jr z, exit_loop
+
+		jr pack_bcd_loop
 	
+	exit_loop:
+		nop
 
-	
-	
+ret
 
-
+; Convert ascii to BCD
+; output a -> BCD
+asc_bcd:
+	; call brack	; Check that char is 0-9
+	; jp nz,illegal	; Exit if illegal char
+	ld a,(hl)	; get char
+	and $0F		; mash high nibble
+	ld (hl),a	; write char
+ret
